91762564fff8f02abdcfc2a1ded542fb52f4b04b
awhit <awhitcomb2050@gmail.com>
2026-03-26 11:24:42 +0100

    working slope 3/26/2026 auto files added will need tuning no more pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/bigtriangleblue.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous
+public class bigtriangleblue  extends LinearOpMode {
+    private DcMotorEx leftFront, rightFront, leftRear, rightRear;
+    private DcMotorEx shootyMotor1, shootyMotor2;
+    private DcMotorEx intankeMotor;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        shootyMotor1 = hardwareMap.get(DcMotorEx.class, "shootyMotor1");
+        shootyMotor2 = hardwareMap.get(DcMotorEx.class, "shootyMotor2");
+        intankeMotor = hardwareMap.get(DcMotorEx.class, "intankeMotor");
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftRear.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightRear.setDirection(DcMotorEx.Direction.FORWARD);
+
+
+
+
+        waitForStart();
+        if (opModeIsActive()) {
+            leftFront.setPower(-.6);
+            rightFront.setPower(-.6);
+            leftRear.setPower(-.6);
+            rightRear.setPower(-.6);
+            sleep(1000);
+            shootyMotor1.setPower(1);
+            shootyMotor2.setPower(1);
+            intankeMotor.setPower(-1);
+
+            sleep(1000);
+            stop();
+
+
+        }
+    }
+}
