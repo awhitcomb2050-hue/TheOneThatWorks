53f59002d796a8fdf7f58d8bc98bf04b8e54f1d9
awhit <awhitcomb2050@gmail.com>
2026-04-09 20:18:43 +0200

    half working pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/i_want_to_die/Constants.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.i_want_to_die;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName("leftFront")
+            .rightFrontMotorName("rightFront")
+            .leftRearMotorName("leftRear")
+            .rightRearMotorName("rightRear")
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(36.9)  // Tune with velocity tuner
+            .yVelocity(26.7);
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .leftFrontMotorName("frontLeft")
+            .rightFrontMotorName("frontRight")
+            .leftRearMotorName("backLeft")
+            .rightRearMotorName("backRight")
+//            .leftFrontEncoderDirection(com.pedropathing.localization.Encoder.Direction.FORWARD)
+//            .rightFrontMotorDirection(com.pedropathing.localization.Encoder.Direction.REVERSE)
+//            .leftRearMotorDirection(com.pedropathing.localization.Encoder.Direction.FORWARD)
+//            .rightRearMotorDirection(com.pedropathing.localization.Encoder.Direction.REVERSE)
+            .forwardTicksToInches(1.0)
+            .turnTicksToInches(1.0)
+//            .turnTicksToInches(1.0)
+            .robotLength(18.0)
+            .robotWidth(18.0);
+
+
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .build();
+
+    }
+}
