fdf31473b56d41ce21f9fb22f5b86db3f19d2557
amelia <awhitcomb2050@gmail.com>
2026-06-21 17:39:32 +0200

    mecanum

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/Mecanum.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.functions;
+
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Mecanum {
+
+    public DcMotorEx leftFront, rightFront, leftRear, rightRear;
+
+    public Mecanum(HardwareMap hardwareMap) {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftRear.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightRear.setDirection(DcMotorEx.Direction.FORWARD);
+
+        leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void drive(double y, double x, double rx) {
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        leftFront.setPower(frontLeftPower);
+        leftRear.setPower(backLeftPower);
+        rightFront.setPower(frontRightPower);
+        rightRear.setPower(backRightPower);
+    }
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TheOneThatWorksTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TheOneThatWorksTeleOp.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class TheOneThatWorksTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.functions.Mecanum;
+
+@TeleOp(name="TheOneThatWorksTeleOp")
+public class TheOneThatWorksTeleOp extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Mecanum drive = new Mecanum(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // forward positive
+            double x = -gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            drive.drive(y, x, rx);
+
+        }
+    }
 }
